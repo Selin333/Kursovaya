a369020095cc8f531a616b1cce3d0bdbdbe5b68c
MelmanDen <zzz-danil-zzz@mail.ru>
2022-05-28 23:58:25 +0300

    я расписал тебе как она должна выглядеть оно работает збс получает tuple где есть путь к файлу

--- dragdrop.py
+++ dragdrop.py
@@ -1,7 +1,27 @@
+import sys
+
 import PyQt5.QtWidgets
+from PyQt5.QtWidgets import QDialog, QPushButton, QApplication
 
 
-def knipka1(self):
-    res=PyQt5.QtWidgets.QFileDialog.getOpenFileName(self,'Open File', 'D:\\111\\Kursovaya','txt file(*.txt)')
+class DlgMain(QDialog):
+    def __init__(self):
+        super().__init__()
 
-knipka1()
+        self.resize(200,200)
+
+        self.btn = QPushButton('Open file', self)
+        self.btn.move(35,50)
+        self.btn.clicked.connect(self.knipka1)
+
+
+
+    def knipka1(self):
+        res = PyQt5.QtWidgets.QFileDialog.getOpenFileName(self,'Open File', 'D:\\111\\Kursovaya','txt file(*.txt)')
+        print(res)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    dlgMain = DlgMain()
+    dlgMain.show()
+    sys.exit(app.exec_())
