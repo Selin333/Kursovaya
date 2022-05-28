1f765ef3c831f44df066eb4f849c1d30e34ecc4f
Selin333 <mail4studying@yandex.ru>
2022-05-28 20:07:14 +0300

    Получение всего письма

--- main.py
+++ main.py
@@ -53,5 +53,41 @@ def pochta_otpravka():
         mail.quit()
         print('Письмо отправлено')
     except:
-        print('errror')
+        print('error')
 # pochta_otpravka()
+def pochta_read():
+    mail = imaplib.IMAP4_SSL('imap.yandex.ru')
+    mail.login('danyamelman@yandex.ru', 'roHB5m8kmy9sdKUm52QoAX2')
+
+    mail.list()
+    mail.select("inbox")
+    result, data = mail.search(None, "ALL")
+
+    ids = data[0]
+    id_list = ids.split()
+    latest_email_id = id_list[-1]
+
+    result, data = mail.fetch(latest_email_id, "(RFC822)")
+    raw_email = data[0][1]
+    raw_email_string = raw_email.decode('utf-8')
+
+    email_message = email.message_from_string(raw_email_string)
+    print(email_message)
+
+    # print(email_message['To'])
+    # print(email.utils.parseaddr(email_message['From']))
+    # print(email_message['Date'])
+    # print(email_message['Subject'])
+    # print(email_message['Message-Id'])
+    # print(email_message['Attachment'])
+
+    email_message = email.message_from_string(raw_email_string)
+
+    if email_message.is_multipart():
+        for payload in email_message.get_payload():
+            body = payload.get_payload(decode=True).decode('utf-8')
+            print(body)
+    else:
+        body = email_message.get_payload(decode=True).decode('utf-8')
+        print(body)
+pochta_read()
